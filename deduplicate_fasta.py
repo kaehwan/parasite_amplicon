b474658772ec7a626ce1a72a2b0715ef7623f6f2
kaehwan <love01007@hotmail.com>
2023-04-11 16:34:20 +0800

    fix bugs

--- deduplicate_fasta.py
+++ deduplicate_fasta.py
@@ -6,8 +6,10 @@ import re
 from Bio import SeqIO
 from Bio.Seq import reverse_complement
 
-## define functions
+# define functions
 # to parse fasta file
+
+
 def parsing_fasta(input_file):
     # read fasta entries and sequences
     entries, sequences = [], []
@@ -18,18 +20,23 @@ def parsing_fasta(input_file):
     return entries, sequences
 
 # to write output fasta file
+
+
 def write_out(output_file, starting_entries, starting_sequences, curated_sequences):
     print('\n-------\nfiltered sequences = %d from %d starting sequences\nresulting sequence = %d sequences\n-------\n' % (
         len(starting_entries) - len(curated_sequences), len(starting_entries), len(curated_sequences)))
     with open(output_file, 'w') as out:
-        entry = starting_entries[starting_sequences.index(curated_sequences[0])]
+        entry = starting_entries[starting_sequences.index(
+            curated_sequences[0])]
         curated_entries = [entry]
         out.write('%s\n%s' % (entry, curated_sequences[0]))
         for i in range(1, len(curated_sequences)):
-            entry = starting_entries[starting_sequences.index(curated_sequences[i])]
+            entry = starting_entries[starting_sequences.index(
+                curated_sequences[i])]
             curated_entries.append(entry)
             out.write('\n%s\n%s' % (entry, curated_sequences[i]))
-    deleted = list(set([entry for entry in starting_entries if entry not in curated_entries]))
+    deleted = list(
+        set([entry for entry in starting_entries if entry not in curated_entries]))
     if len(deleted) > 0:
         with open('removed_fasta_entries.txt', 'w') as out:
             out.write('%s\n' % deleted[0])
@@ -37,6 +44,8 @@ def write_out(output_file, starting_entries, starting_sequences, curated_sequenc
                 out.write('\n%s\n' % deleted[i])
 
 # to generate one k-mer
+
+
 def kmer_gen(sequence, k, start=0):
     # sequence is a fasta sequence
     # k is the length of kmer
@@ -45,21 +54,24 @@ def kmer_gen(sequence, k, start=0):
     return kmer
 
 # to cleanup empty sequence(s) in list after curation
+
+
 def cleanup(_list):
     # _list is the list of sequences generated during curating
     while '' in _list:
         _list.remove('')
     return _list
 
+
 def derep_longest(input_file, sequence_type='n'):
     starting_entries, starting_sequences = parsing_fasta(input_file)
     if len(starting_entries) < 1:
         sys.exit('\n\nInvalid input file\n\n')
-    
+
     editing = list(set(starting_sequences))
     editing.sort(key=len)
 
-    for i in range(len(editing) - 1):
+    for i in range((len(editing) - 1)):
         if len(editing[i]) >= minimum:
             comparing = editing[i]
             kmer = kmer_gen(comparing, k=length, start=0)
@@ -77,20 +89,21 @@ def derep_longest(input_file, sequence_type='n'):
     editing = cleanup(editing)
     write_out(output_file, starting_entries, starting_sequences, editing)
 
+
 parser = argparse.ArgumentParser(prog='\n\nSequence Database Dereplicator program',
                                  usage='\n%(prog)s : dereplicates nucleotide database'
-                                       'from a list of sequences (by exact match).\n\n')
-parser.add_argument('-mode', dest='mode', required=True, choices=['derep'], 
+                                 'from a list of sequences (by exact match).\n\n')
+parser.add_argument('-mode', dest='mode', required=True, choices=['derep'],
                     help='dereplicate a fasta file with multiple entries')
-parser.add_argument('-in', dest='input_file', type=argparse.FileType('r'), required=True, 
+parser.add_argument('-in', dest='input_file', type=argparse.FileType('r'), required=True,
                     help='Input file containing fasta entries')
-parser.add_argument('-out', dest='output_file', required=True, 
+parser.add_argument('-out', dest='output_file', required=True,
                     help='Your output file')
-parser.add_argument('-n', dest='database', action='store_const', const='n', 
+parser.add_argument('-n', dest='database', action='store_const', const='n',
                     help='nucleotide sequences')
-parser.add_argument('-len', dest='length', default=30, type=int, 
+parser.add_argument('-len', dest='length', default=30, type=int,
                     help='length of k-mer')
-parser.add_argument('-min_length', dest='minimum', default=1, type=int, 
+parser.add_argument('-min_length', dest='minimum', default=1, type=int,
                     help='minimum sequence length in your data (for dereplication)')
 args = parser.parse_args()
 
@@ -103,8 +116,8 @@ database = args.database
 length = args.length
 minimum = args.minimum
 
-if mode=='derep':
-    derep_longest(input_file, database)
+if mode == 'derep':
+    derep_longest(input_file, sequence_type=database)
     print('Author\t: %s\nDate\t: %s' % (
         'Sim Kae Hwan', 'April 11, 2023'
     ))
